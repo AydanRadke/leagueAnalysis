02845d50c61ead5c5df3b67b122470ba1a339669
AydanRadke <168686865+AydanRadke@users.noreply.github.com>
2024-05-01 23:14:54 -0500

    Update startHere.py
    Made this code more readable. Still trying to figure out how GitHub works

--- startHere.py
+++ startHere.py
@@ -6,27 +6,24 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
     (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
 
-res = requests.get('https://www.op.gg/summoners/na/Electricpotato76-NA1', headers=headers, allow_redirects=False)
-res.raise_for_status()
-# print(res.text)
-for resp in res.history:
-    print(resp.status_code, resp.url)
+username = "Electricpotato76-NA1"
+url = "https://www.op.gg/summoners/na/" + username
+webpage = requests.get(url, headers=headers, allow_redirects=False)
+webpage.raise_for_status()
 
 # This changes the html into a readable enough string.
-onlineThing = bs4.BeautifulSoup(res.text, 'html.parser')
-onlineThing = onlineThing.prettify()
+webpage = bs4.BeautifulSoup(webpage.text, 'html.parser')
+webpage = webpage.prettify()
 
 # We get to start the fun part!!! Since we have a string, we are going to
 # Split the string up game by game, and collect the data we need to analyze
 games = []
 # Splits the long html string up game by game
 while True:
-    start_games_index = onlineThing.find('"participants"')
-    end_games_index = onlineThing[start_games_index + 1:].find('"participants"')
-    games.append(onlineThing[start_games_index:start_games_index + end_games_index])
-    onlineThing = onlineThing[start_games_index + end_games_index:]
-    print(start_games_index)
-    print(end_games_index)
+    start_games_index = webpage.find('"participants"')
+    end_games_index = webpage[start_games_index + 1:].find('"participants"')
+    games.append(webpage[start_games_index:start_games_index + end_games_index])
+    webpage = webpage[start_games_index + end_games_index:]
     if end_games_index == -1:
         break
 
